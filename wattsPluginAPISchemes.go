db31b5650a7849015f29cf9cbb6d184c5d169045
Lukas Burgey <ubedv@student.kit.edu>
2017-12-07 11:19:11 +0100

    Rename a confusingly named scheme

--- wattsPluginAPISchemes.go
+++ wattsPluginAPISchemes.go
@@ -1,3 +1,4 @@
+// Package schemes declares govalid schemes to check watts plugin API input and output.
 package schemes
 
 import (
@@ -11,19 +12,23 @@ const (
 
 var (
 	schemeAccessToken = v.Optional(v.String())
-	schemeUserInfo    = v.Object(
+
+	schemeUserInfo = v.Object(
 		v.ObjKV("iss", v.String()),
 		v.ObjKV("sub", v.String()),
 	)
+
 	schemeAdditionalLogins = v.Array(v.ArrEach(
 		v.Object(
 			v.ObjKV("user_info", schemeUserInfo),
 			v.ObjKV("access_token", schemeAccessToken),
 		),
 	))
+
 	schemeParams = v.Object(
 		v.ObjKeys(v.String(v.StrRegExp("^[a-z0-9_]+$"))),
 	)
+
 	schemeCredential = v.Object(
 		v.ObjKV("name", v.String()),
 		v.ObjKV("type", v.String()),
@@ -32,6 +37,7 @@ var (
 		v.ObjKV("rows", v.Optional(v.Number())),
 		v.ObjKV("cols", v.Optional(v.Number())),
 	)
+
 	schemeRequestParam = v.Object(
 		v.ObjKV("key", v.String()),
 		v.ObjKV("name", v.String()),
@@ -40,29 +46,12 @@ var (
 		v.ObjKV("mandatory", v.Boolean()),
 	)
 
-	// PluginInputScheme Scheme to check the input of a plugin against
-	PluginInputScheme = v.Or(
-		v.Object(
-			v.ObjKV("action", v.String(v.StrIs("parameter"))),
-			v.ObjKV("watts_version", v.String()),
-		),
-		v.Object(
-			v.ObjKV("action", v.String()),
-			v.ObjKV("watts_version", v.String()),
-			v.ObjKV("watts_userid", v.String()),
-			v.ObjKV("cred_state", v.String()),
-			v.ObjKV("access_token", schemeAccessToken),
-			v.ObjKV("additional_logins", schemeAdditionalLogins),
-			v.ObjKV("conf_params", schemeParams),
-			v.ObjKV("params", schemeParams),
-			v.ObjKV("user_info", schemeUserInfo),
-		),
-	)
 	schemeRequestResultValue = v.Object(v.ObjKV("result", v.Or(
 		v.String(v.StrIs("error")),
 		v.String(v.StrIs("oidc_login")),
 		v.String(v.StrIs("ok")),
 	)))
+
 	schemesRequest = map[string]v.Validator{
 		"error": v.Object(
 			v.ObjKV("result", v.String(v.StrIs("error"))),
@@ -81,8 +70,27 @@ var (
 		),
 	}
 
-	// WattsSchemes Scheme to validate specific input from watts against
-	WattsSchemes = map[string](map[string]v.Validator){
+	// PluginInputScheme is used to validate the input of a plugin
+	PluginInputScheme = v.Or(
+		v.Object(
+			v.ObjKV("action", v.String(v.StrIs("parameter"))),
+			v.ObjKV("watts_version", v.String()),
+		),
+		v.Object(
+			v.ObjKV("action", v.String()),
+			v.ObjKV("watts_version", v.String()),
+			v.ObjKV("watts_userid", v.String()),
+			v.ObjKV("cred_state", v.String()),
+			v.ObjKV("access_token", schemeAccessToken),
+			v.ObjKV("additional_logins", schemeAdditionalLogins),
+			v.ObjKV("conf_params", schemeParams),
+			v.ObjKV("params", schemeParams),
+			v.ObjKV("user_info", schemeUserInfo),
+		),
+	)
+
+	// PluginOutputScheme is used to validate the output of a plugin
+	PluginOutputScheme = map[string](map[string]v.Validator){
 		"1.0.0": map[string]v.Validator{
 			"parameter": v.Object(
 				v.ObjKV("result", v.String(v.StrIs("ok"))),
